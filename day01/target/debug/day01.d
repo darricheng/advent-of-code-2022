63687fe2a876ee926ee3817c8df4e8096db0da77
Darric Heng <77005864+darricheng@users.noreply.github.com>
2022-12-05 23:35:36 +0800

    Remove target folder

--- day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/Users/darricheng/code/advent-of-code-2022/day01/target/debug/day01: /Users/darricheng/code/advent-of-code-2022/day01/src/main.rs
--- day01/target/debug/deps/day01-e9d79eaf428fc77c.d
+++
@@ -1,5 +0,0 @@
-/Users/darricheng/code/advent-of-code-2022/day01/target/debug/deps/day01-e9d79eaf428fc77c: src/main.rs
-
-/Users/darricheng/code/advent-of-code-2022/day01/target/debug/deps/day01-e9d79eaf428fc77c.d: src/main.rs
-
-src/main.rs:
